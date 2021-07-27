003c32796da19333626e8a5fff106a1ab0b7def2
lutzer <lutz@lu-re.de>
2021-07-28 01:40:14 +0200

    added initial peripheral initialisation

--- src/main.rs
+++ src/main.rs
@@ -10,61 +10,27 @@ use stm32f1xx_hal::{
     prelude::*, 
 };
 
-// mod usart;
 mod peripherals;
 use peripherals::{Peripherals};
 
 #[allow(unused_imports)]
 use panic_halt; // When a panic occurs, stop the microcontroller
 
-// This marks the entrypoint of our application. The cortex_m_rt creates some
-// startup code before this, but we don't need to worry about this
 #[entry]
 fn main() -> ! {
 
     let peripherals = Peripherals::init();
 
-    // let dp = pac::Peripherals::take().unwrap();
-    // let cp = cortex_m::Peripherals::take().unwrap();
-    // let mut rcc = dp.RCC.constrain();
-
-    // // access PGIOC and PGIOB registers and prepare the alternate function I/O registers
-    // let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
-    // let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
-    // let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
-
-    // // set clock frequency to internal 8mhz oscillator
-    // let mut flash = dp.FLASH.constrain();
-    // let clocks = rcc.cfgr.sysclk(8.mhz()).freeze(&mut flash.acr);
-
-    // // configure pc13 as output
-    // let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-
-    // //usart1
-    // let tx = gpioa.pa9.into_alternate_push_pull(&mut gpioa.crh);
-    // let rx = gpioa.pa10;
-    // let mut serial = Serial::usart1(
-    //     dp.USART1,
-    //     (tx, rx),
-    //     &mut afio.mapr,
-    //     Config::default().baudrate(115200.bps()),
-    //     clocks,
-    //     &mut rcc.apb2,
-    // );
-
-    // uses System Timer peripheral to create delay function
-    // let mut delay = Delay::new(cp.SYST, clocks);
-
     let mut led = peripherals.led.unwrap();
     let mut delay = peripherals.delay.unwrap();
+    let mut serial = peripherals.usart1.unwrap();
 
     loop {
         led.set_high().ok();
         delay.delay_ms(100 as u32);
         led.set_low().ok();
         delay.delay_ms(100 as u32);
-        // let sent = b'T';
-        // serial.write(sent).ok();
-
+        let sent = b'T';
+        serial.write(sent).ok();
     }
 }
--- src/peripherals.rs
+++ src/peripherals.rs
@@ -1,5 +1,3 @@
-use cortex_m_rt;
-
 use stm32f1xx_hal::{
   delay::Delay, 
   pac, 
@@ -30,45 +28,46 @@ impl Peripherals {
     let cp = cortex_m::Peripherals::take().unwrap();
 
     // set clock frequency to internal 8mhz oscillator
-    let mut rcc = dp.RCC.constrain();
+    let rcc = dp.RCC.constrain();
     let mut flash = dp.FLASH.constrain();
     let clocks = rcc.cfgr.sysclk(8.mhz()).freeze(&mut flash.acr);
 
     // access PGIOC and PGIOB registers and prepare the alternate function I/O registers
-    let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
-    let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
-    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+    let mut apb2 = rcc.apb2;
+    let gpioc = dp.GPIOC.split(&mut apb2);
+    let gpioa = dp.GPIOA.split(&mut apb2);
+    let afio = dp.AFIO.constrain(&mut apb2);
 
     return Peripherals{
-      led: Some(gpioc.pc13.into_push_pull_output(&mut gpioc.crh)),
+      led: Peripherals::init_led(gpioc),
       delay: Some(Delay::new(cp.SYST, clocks)),
-      usart1: None
+      usart1: Peripherals::init_usart1(dp.USART1, gpioa, afio, &clocks, apb2)
     }
   }
 
-  // fn init_led(gpioc: &mut gpio::gpioc::Parts) -> Option<GpioOutput> {
-  //   let led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-  //   return Some(led);
-  // }
+  fn init_led(mut gpioc: stm32f1xx_hal::gpio::gpioc::Parts) -> Option<GpioOutput> {
+    let led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+    return Some(led);
+  }
 
-  // fn init_usart1(
-  //   dp: &pac::Peripherals, 
-  //   gpioa: &mut gpio::gpioa::Parts, 
-  //   afio: &mut afio::Parts, 
-  //   clocks: &stm32f1xx_hal::rcc::Clocks, 
-  //   rcc: &mut stm32f1xx_hal::rcc::Rcc
-  // ) -> Option<Usart1Serial> {
-  //   let tx = gpioa.pa9.into_alternate_push_pull(&mut gpioa.crh);
-  //   let rx = gpioa.pa10;
+  fn init_usart1(
+    usart1: USART1, 
+    mut gpioa: gpio::gpioa::Parts, 
+    mut afio: afio::Parts, 
+    clocks: &stm32f1xx_hal::rcc::Clocks, 
+    mut apb2: stm32f1xx_hal::rcc::APB2
+  ) -> Option<Usart1Serial> {
+    let tx = gpioa.pa9.into_alternate_push_pull(&mut gpioa.crh);
+    let rx = gpioa.pa10;
 
-  //   let serial = Serial::usart1(
-  //     dp.USART1,
-  //     (tx, rx),
-  //     &mut afio.mapr,
-  //     Config::default().baudrate(115200.bps()),
-  //     *clocks,
-  //     &mut rcc.apb2,
-  //   );
-  //   return Some(serial);
-  // }
+    let serial = Serial::usart1(
+      usart1,
+      (tx, rx),
+      &mut afio.mapr,
+      Config::default().baudrate(115200.bps()),
+      *clocks,
+      &mut apb2,
+    );
+    return Some(serial);
+  }
 }
